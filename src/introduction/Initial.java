a0c5dfc789315e4562e799fd7f7c0db1313503f2
Agris Traskovs <agris.traskovs@gmail.com>
2021-09-07 08:26:16 +0300

    initial commit from my local computer

---
+++ src/introduction/Initial.java
@@ -0,0 +1,16 @@
+package introduction;
+
+
+public class Initial {
+
+    public static void main(String args[]) {
+        System.out.println("Hello, world, from Agris");
+
+
+        for (String s: args) {
+            System.out.println(s);
+        }
+
+    }
+
+}
