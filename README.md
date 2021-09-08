78e0a201f08df8053d9fc3208fcaeaeac30d26cc
Agris Traskovs <agris.traskovs@gmail.com>
2021-09-08 09:55:02 +0300

    basic variables example class

--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 This is an intro test repo, to show a simple code base.
 For git commands you can see : https://education.github.com/git-cheat-sheet-education.pdf
 
+For git security setup: https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token
 
 This will be a testable code base where some introduction java code will live. 
 Intro code:
-  * To be added later
+  * Hello world + input args output initial 
+  * Basic variables intro class
 
 
---
+++ src/variables/BasicVariables.java
@@ -0,0 +1,33 @@
+package variables;
+
+public class BasicVariables {
+
+
+    public static void main(String[] args) {
+
+        int wholeNumber = 123;
+        int otherNumber = 13;
+
+        short shortNumber = 123;
+
+
+        boolean truthOrFalsehood; // what is the value?
+
+        boolean isShortEqualToInt = wholeNumber == shortNumber; // == means is it equal, used only for primitive variables
+
+        truthOrFalsehood = wholeNumber > otherNumber;
+
+        System.out.println(" whole number result : " + wholeNumber);
+        System.out.println("Summation: " );
+        System.out.println(otherNumber + wholeNumber);
+
+        System.out.println(truthOrFalsehood);
+
+        System.out.println(isShortEqualToInt);
+        System.out.println(" Some interesting things that we can try? : " + wholeNumber + otherNumber);
+
+    }
+
+
+
+}
