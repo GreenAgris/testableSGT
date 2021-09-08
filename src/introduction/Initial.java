f268b9a8552e0924877937989ab9ba1ab97965f3
anna-lice <90284122+anna-lice@users.noreply.github.com>
2021-09-08 17:15:49 +0100

    Update Initial.java

--- src/introduction/Initial.java
+++ src/introduction/Initial.java
@@ -6,6 +6,7 @@ public class Initial {
     public static void main(String args[]) {
         System.out.println("Hello, world, from Agris");
         System.out.println("Hello, world, from Oksana");
+        System.out.println("Hello, world, from Anna");
 
         for (String s: args) {
             System.out.println(s);
