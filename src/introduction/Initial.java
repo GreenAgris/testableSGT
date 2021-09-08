3d4afdc8678e6f7d90a1ea7318cc011f8f86da6d
KristineGlinina <90214214+KristineGlinina@users.noreply.github.com>
2021-09-08 19:00:16 +0300

    Update Initial.java

--- src/introduction/Initial.java
+++ src/introduction/Initial.java
@@ -5,7 +5,7 @@ public class Initial {
 
     public static void main(String args[]) {
         System.out.println("Hello, world, from Agris");
-
+        System.out.println("and from Kristine");
 
         for (String s: args) {
             System.out.println(s);
