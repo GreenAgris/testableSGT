9b047c1257d33ea09578e2cefb57ec12a5908976
baibamatule <90314162+baibamatule@users.noreply.github.com>
2021-09-08 18:59:40 +0300

    Update Initial.java

--- src/introduction/Initial.java
+++ src/introduction/Initial.java
@@ -5,6 +5,7 @@ public class Initial {
 
     public static void main(String args[]) {
         System.out.println("Hello, world, from Agris");
+        System.out.println("Hello, world, from Baiba");
 
 
         for (String s: args) {
