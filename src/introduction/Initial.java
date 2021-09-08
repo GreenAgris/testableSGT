76d86d95ba168d759b4b55fe1cd935ab52fa9818
dpundure <90337954+dpundure@users.noreply.github.com>
2021-09-08 19:00:48 +0300

    Added extra line from Dace

--- src/introduction/Initial.java
+++ src/introduction/Initial.java
@@ -4,8 +4,8 @@ package introduction;
 public class Initial {
 
     public static void main(String args[]) {
-        System.out.println("Hello, world, from Agris");
-
+        System.out.println("Hello, world, from Agris");    
+        System.out.println("Hello, world, from Dace");
 
         for (String s: args) {
             System.out.println(s);
