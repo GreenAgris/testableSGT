4f87165d98dcca8a5645887f40cb9182e18dbab5
Ausma <ausma.kalnina5@gmail.com>
2021-09-13 20:32:17 +0300

    Homework from Day 3

--- src/variables/ReferenceVariables.java
+++ src/variables/ReferenceVariables.java
@@ -1,7 +1,35 @@
 package variables;
 
-public class ReferenceVariables { //If needed, you can use this as the base class for Homework
 
 
 
-}
+    public class ReferenceVariables {
+
+        public static void main(String[] args) {
+
+            int cm = 1;
+            double inch = cm / 2.54;
+
+            System.out.println (cm + "cm =");
+            System.out.println(inch + "inch");
+
+            double ft = cm / 30.48;
+            System.out.println(cm + " cm = ");
+            System.out.println(ft + " ft");
+
+            double m = cm * 100;
+
+            double yards = 0.9144 * m;
+
+            System.out.println(" meter = ");
+            System.out.println(yards + " yards");
+
+        }
+    }
+
+
+
+
+
+
+
