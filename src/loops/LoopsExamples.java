da8f43e27a2581d1bdd657ae39ceeef56e3cf5a6
greenAgris <agris.traskovs@gmail.com>
2021-09-26 11:19:11 +0300

    never to be published

--- src/loops/LoopsExamples.java
+++ src/loops/LoopsExamples.java
@@ -1,5 +1,10 @@
 package loops;
 
+import static util.MathHelpers.isPrime;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class LoopsExamples {
 
     int[] intArray = {16, 20, 21, 23, 23, 12, 71};
@@ -10,15 +15,15 @@ public class LoopsExamples {
     }
 
     public static int sumInt(int[] input) {
-        return -1;
+        return Arrays.stream(input).sum();
     }
 
     public static int findLargest(int[] input) {
-        return -1;
+        return Arrays.stream(input).max().getAsInt();
     }
 
     public static int multiplyOnlyEvenNumbers(int[] input) {
-        return -1;
+        return Arrays.stream(input).filter(element -> element % 2 == 0).reduce(1, Math::multiplyExact);
     }
 
     //Advanced exercises
@@ -30,21 +35,33 @@ public class LoopsExamples {
     //4  4  8  12  16
     //System.out.printf("%4d",number); will come in handy here - it formats more neatly and does not insert a new line
     public static void printOutCalculationTable(int maxNumber) {
-        System.out.println("TODO");
+        for (int i = 1; i <= maxNumber; i++) {
+            for (int j = 1; j <= maxNumber; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
     }
 
     public static void printOutCalculationTable(int minNumber, int maxNumber) {
-        System.out.println("TODO");
+        for (int i = minNumber; i <= maxNumber; i++) {
+            for (int j = minNumber; j <= maxNumber; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
     }
 
     public static String[] findSomethingInText(String search, String[] testText) {
-        return new String[3];
+        var initial = Arrays.stream(testText).filter(element -> element.contains(search)).collect(Collectors.toList());
+        String[] array = new String[initial.size()];
+        return initial.toArray(array);
     }
 
 
     //Complex exercises
     public static int findFirstPrimeNumber(int[] input) { // returns first found prime number or -1 if none are found
-        return -1;
+        return Arrays.stream(input).filter(element -> isPrime(element)).findFirst().orElse(-1);
     }
 
 
