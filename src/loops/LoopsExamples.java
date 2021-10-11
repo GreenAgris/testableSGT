7a927b9e27beaf68e57570b0b4d5e800f462a2c2
Vija-M <vija.riga@gmailcom>
2021-10-11 16:51:39 +0300

    ClassTask

--- src/loops/LoopsExamples.java
+++ src/loops/LoopsExamples.java
@@ -1,19 +1,29 @@
 package loops;
 
+import java.util.Arrays;
+
 public class LoopsExamples {
 
-    static int[] intArray = {16, 20, 21, 23, 23, 12, 71};
+
+    static int[] intArray = {16, 20, 21, 9, 12, 14, 22, 23, 7};
 
 
     static String[] exampleText = new String[]{"seastar", "Patrick", "Patrick seastar", "Omnitricks", "Omnibus", "Stellaris"};
 
     public static void main(String[] args) {
-        System.out.println("Final result: " + sumInt(intArray));
+        System.out.println("Final sum result: " + sumInt(intArray));
+        System.out.println("");
+        System.out.println("Final largest number: " + findLargest(intArray));
+        System.out.println("");
+        System.out.println("Final multiply even numbers result: " + multiplyOnlyEvenNumbers(intArray));
+        System.out.println("");
+        System.out.println(findSomethingInText("Omni", exampleText));
+        System.out.println("First prime number in array or -1: " + findFirstPrimeNumber(intArray));
+           }
 
-    }
     public static int sumInt(int[] input) {
         int sumOfArray = 0;
-        for (int element : input) {
+        for ( int element : input ) {
             sumOfArray += element;
             System.out.println("Current sum : " + sumOfArray);
         }
@@ -22,11 +32,26 @@ public class LoopsExamples {
 
 
     public static int findLargest(int[] input) {
-        return -1;
+        int largestNumber = intArray[0];
+        for ( int element : input ) {
+            if (element > largestNumber) {
+                largestNumber = element;
+            }
+            System.out.println("Largest number : " + largestNumber);
+        }
+        return largestNumber;
     }
 
+
     public static int multiplyOnlyEvenNumbers(int[] input) {
-        return -1;
+        int result = 1;
+        for ( int element : input ) {
+            if (element % 2 == 0) {
+                result = result * element;
+            }
+            System.out.println("Multiply even numbers result : " + result);
+        }
+        return result;
     }
 
     //Advanced exercises
@@ -37,23 +62,57 @@ public class LoopsExamples {
     //3  3  6  9  12
     //4  4  8  12  16
     //System.out.printf("%4d",number); will come in handy here - it formats more neatly and does not insert a new line
+
     public static void printOutCalculationTable(int maxNumber) {
-        System.out.println("TODO");
+        for ( int i = 1; i <= maxNumber; i++ ) {
+            for ( int j = 1; j <= maxNumber; j++ ) {
+                System.out.format("%4d", i * j);
+            }
+            System.out.println();
+        }
     }
 
     public static void printOutCalculationTable(int minNumber, int maxNumber) {
-        System.out.println("TODO");
+        for ( int i = minNumber; i <= maxNumber; i++ ) {
+            for ( int j = minNumber; j <= maxNumber; j++ ) {
+                System.out.format("%4d", i * j);
+            }
+            System.out.println();
+        }
     }
 
     public static String[] findSomethingInText(String search, String[] testText) {
-        return new String[3];
+        String[] newText = new String[0];
+        for ( String element : testText ) {
+            if (element.contains(search)) {
+                newText = Arrays.copyOf(newText, newText.length + 1);
+                newText[newText.length - 1] = element;
+            }
+            System.out.println(Arrays.toString(newText));
+        }
+
+        return newText;
     }
 
 
     //Complex exercises
     public static int findFirstPrimeNumber(int[] input) { // returns first found prime number or -1 if none are found
-        return -1;
-    }
+        int result = -1;
+        for ( int i = 0; i < input.length; i++ ) {
+            boolean prime = true;
+            for ( int j = 2; j < input[i]; j++ ) {
+                if (input[i] % j == 0) {
+                    prime = false;
+                }
+            }
 
+            if (prime == true) {
+                result = input[i];
+                break;
+
+            }
+        }
+        return result;
+    }
 
 }
