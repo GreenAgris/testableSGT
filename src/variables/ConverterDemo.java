96c2c16e0db0829e1a5eec9ae5d26ff496d916e7
Vija-M <vija.riga@gmailcom>
2021-10-11 16:23:39 +0300

    ClassTask

---
+++ src/variables/ConverterDemo.java
@@ -0,0 +1,25 @@
+package variables;
+
+public class ConverterDemo {
+    public static void main(String[] args) {
+        Converter converter1 = new Converter(1, 1, 1);
+        converter1.printOutConvertCmToYards();
+        converter1.printOutConvertCmToFeet();
+        converter1.printOutConvertCmToInches();
+        converter1.printOutConvertMetersToYards();
+        converter1.printOutConvertMetersToFeet();
+        converter1.printOutConvertMetersToInches();
+        converter1.printOutConvertKilometerToMiles();
+        System.out.println();
+
+       Converter converter2 = new Converter(22, 86, 36);
+        converter2.printOutConvertCmToYards();
+        converter2.printOutConvertCmToFeet();
+        converter2.printOutConvertCmToInches();
+        converter2.printOutConvertCmToFeet();
+        converter2.printOutConvertMetersToYards();
+        converter2.printOutConvertMetersToFeet();
+        converter2.printOutConvertMetersToInches();
+        converter2.printOutConvertKilometerToMiles();
+    }
+}
