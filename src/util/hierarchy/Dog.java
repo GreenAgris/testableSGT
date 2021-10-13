ecf69c3a2f1f4e5f405ec6e39d3f58637a66d82c
Vija-M <vija.riga@gmailcom>
2021-10-13 19:53:19 +0300

    ClassTask Barking dogs don't bite

--- src/util/hierarchy/Dog.java
+++ src/util/hierarchy/Dog.java
@@ -1,20 +1,19 @@
 package util.hierarchy;
 
-import java.util.Objects;
-import java.util.StringJoiner;
-
 public class Dog extends Animal {
     String name;
     double height;
     boolean canRun;
     boolean canSwim;
+    boolean canBite;
     String colour;
 
-    public Dog(String name, double height, boolean canRun, boolean canSwim, String colour) {
+    public Dog(String name, double height, boolean canRun, boolean canSwim, boolean canBite, String colour) {
         this.name = name;
         this.height = height;
         this.canRun = canRun;
         this.canSwim = canSwim;
+        this.canBite = canBite;
         this.colour = colour;
     }
 
@@ -23,7 +22,7 @@ public class Dog extends Animal {
     public String makeSound() {
         String result = "tjaf";
         if (height > 18) {
-           result =  "Bark-bark!";
+            result = "Bark-bark!";
         }
         return result;
 
