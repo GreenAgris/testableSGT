76bc7d3e0eec80dd9d8b2fcb84a3ff8f8bdc636b
Vija-M <vija.riga@gmailcom>
2021-10-13 11:17:44 +0300

    HomeTask

--- src/loops/Histogram.java
+++ src/loops/Histogram.java
@@ -1,6 +1,7 @@
 package loops;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Histogram {
     /* Write a histogram (vertical) based on the following approach:
@@ -12,18 +13,27 @@ public class Histogram {
      representing the percentage value. The output values showing percentages are consistent with the first
      line printed axis values. For convinience the percentage numbers should be rounded to whole numbers.
      */
-    static int[] intArray = {10, 2, 2, 56, 6, 4, 70, 0, 30, 20};
+
+    public static int[] createArray() {
+        Random rd = new Random(); // creating Random object
+        int[] intArray = new int[10];
+        for ( int i = 0; i < intArray.length; i++ ) {
+            intArray[i] = rd.nextInt(100); //Generates array randomly - max element value - 100
+        }
+        System.out.println("Generated random array: " + Arrays.toString(intArray));
+        return ( intArray );
+    }
 
     public static void calculatePercentage(int[] input) {
         int sum = 0;
         for ( int k : input ) sum += k;    //calculating sum of array elements
-        System.out.println("Sum of all elements = " + sum);
+        System.out.println("Sum of all array elements = " + sum);
         int percentageArray[] = new int[input.length];  //creating new array with the same length
         for ( int j = 0; j < input.length; j++ )
             percentageArray[j] = input[j] * 100 / sum;      //filling it with a new value - percentages
-        System.out.println("Our array transformed to % value: " + Arrays.toString(percentageArray));
-                System.out.println(String.format("%n"+"                              -//- HISTOGRAM OF PERCENTAGE VALUE -//- %n" + "%n" +
-        "         0         10        11        30        40        50        60        70        80        90       100"));
+        System.out.println("Our array is transformed to % value: " + Arrays.toString(percentageArray));
+        System.out.println(String.format("%n" + "                              -//- HISTOGRAM OF PERCENTAGE VALUE -//- %n" + "%n" +
+                "         0         10        20        30        40        50        60        70        80        90       100"));
         for ( int i = 0; i < percentageArray.length; i++ ) {
             System.out.print(i + ".element ");
             while
@@ -36,13 +46,9 @@ public class Histogram {
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(intArray));
-        calculatePercentage(intArray);
-
+        calculatePercentage(createArray().clone()); //in our function we do use array that is equal to the generated array
     }
-
-
-    }
+}
 
 
 
