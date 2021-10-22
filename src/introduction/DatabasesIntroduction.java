8649d9f2cf70e7271b6fab72ecb6ce396d07595f
greenAgris <agris.traskovs@gmail.com>
2021-10-22 21:47:15 +0300

    db update added Beaver class, code for graphs as well

--- src/introduction/DatabasesIntroduction.java
+++ src/introduction/DatabasesIntroduction.java
@@ -5,18 +5,28 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import util.hierarchy.Beaver;
 
 public class DatabasesIntroduction {
 
     public static void main(String[] args) {
+
+        //                v place to put exception and what to do if things did not work
+        //  try {} catch (){ }
+        //       ^place to write if everything works out
         try {
             Connection connection = DriverManager.getConnection("jdbc:sqlite:/Users/agris.traskovs/code/testableSGT/sql/zoo.db");
+
             Statement statement = connection.createStatement();
-            statement.execute("SELECT name FROM Birds;");
+            //INSERT INTO Birds ( name, age, eyeColor) VALUES( 'Goose', 15, 'black');
+            statement.execute("INSERT INTO Birds ( name, age, eyeColor) VALUES( 'Goose', 15, 'black');");
+
+            statement.execute("SELECT name, age FROM Birds;");
+
             ResultSet rs = statement.getResultSet();
 
             while (rs.next()) {
-                System.out.println(rs.getString("name"));
+                System.out.println(rs.getString("name") + " is " + rs.getInt("age") + " years old.");
             }
             statement.close();
 
@@ -24,4 +34,32 @@ public class DatabasesIntroduction {
             throwables.printStackTrace();
         }
     }
+
+    public static void saveBeaver(Beaver bev) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:/Users/agris.traskovs/code/testableSGT/sql/zoo.db");
+            Statement statement = connection.createStatement();
+            //animal first
+            //int generatedID =
+                statement.execute(
+                "INSERT INTO Animals ( age, eyeColour) VALUES( " + bev.getAge() + ", '" + bev.getEyeColor() + "');");
+            //beaver second
+            String queryLastRowInserted = "SELECT last_insert_rowid() AS id";
+            statement.execute(queryLastRowInserted);
+            ResultSet rs = statement.getResultSet();
+            rs.next();
+            int generatedID = rs.getInt("id");
+
+
+            statement.execute("INSERT INTO Beaver ( lengthForTeeth, colourOfFur, hasADam, animalID) VALUES( "
+                + bev.getLengthOfTeeth() + ", '" + bev.getColourOfFur() + "', "
+                + (bev.isHasADam() ? 1 : 0) + ", " + generatedID + ");");
+
+            statement.close();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
 }
--- src/multiItems/complex/QueuedExample.java
+++ src/multiItems/complex/QueuedExample.java
@@ -1,20 +1,37 @@
 package multiItems.complex;
 
 import java.util.Queue;
+import java.util.Stack;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class QueuedExample {
 
 
     public static void main(String[] args) {
-        Queue<Integer> myQueue = new LinkedBlockingQueue<>();
+       String input1 = "Was it a car or a cat I saw?";
+       String input2 = "Momy!";
+       String input3 = "Yo, 2 banana boy!";
+        isItPalindrome(input1);
+        isItPalindrome(input2);
+        isItPalindrome(input3);
+    }
 
-        myQueue.add(4);
-        myQueue.add(2);
-        myQueue.add(-1);
-        myQueue.add(8);
-
-        System.out.println(myQueue.remove());
-
+    public static void isItPalindrome(String input){
+        Queue<Character> myQueue = new LinkedBlockingQueue<>();
+        Stack<Character> myStack = new Stack<>();
+        System.out.print(input + " ---> ");
+        for (Character ch : input.toLowerCase()
+            .replaceAll("[^a-z]", "").toCharArray()){
+            myQueue.add(ch);
+            myStack.add(ch);
+        }
+        int max = myQueue.size();
+        for (int i = 0; i <  max ; i++) {
+            if (!myQueue.remove().equals(myStack.pop())){
+                System.out.println("Not a palindrome!");
+                return;
+            }
+        }
+        System.out.println("It is a palindrome!");
     }
 }
