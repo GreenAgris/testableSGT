ded612d9cb33fdf179e9de37be9f5cc71c17422f
Vija-M <vija.riga@gmailcom>
2021-11-02 16:53:59 +0200

    Hometasks

---
+++ src/introduction/DBIntroDog.java
@@ -0,0 +1,36 @@
+package introduction;
+
+import util.hierarchy.Beaver;
+import util.hierarchy.Dog;
+
+import java.sql.*;
+
+public class DBIntroDog {
+    public static void saveBeaver(Dog dog) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:/Users/37126/SQLITE3/zoo-v.db");
+            Statement statement = connection.createStatement();
+            //animal first
+            //int generatedID =
+            statement.execute(
+                    "INSERT INTO Animals ( age, eyeColour) VALUES( " + dog.getAge() + ", '" + dog.getEyeColor() + "');");
+            //dog second
+            String queryLastRowInserted = "SELECT last_insert_rowid() AS id;";
+            statement.execute(queryLastRowInserted);
+            ResultSet rs = statement.getResultSet();
+            rs.next();
+            int generatedID = rs.getInt("id");
+
+
+            statement.execute("INSERT INTO Dog ( lengthForTeeth, colourOfFur, hasADam, animalID) VALUES( "
+                    + dog.getName() + ", '"  + dog.getHeight() + ", '" + (dog.isCanRun() ? 1 : 0) + ", '"
+                    + (dog.isCanSwim() ? 1 : 0) + ", '"  + (dog.isCanBite() ? 1 : 0) + ", '"  + dog.getColour() + ", '"  + generatedID + ");");
+
+            statement.close();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+}
